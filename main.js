81f27fd806daaac20fd57d030f2fa6fcd2b8e005
nicola gallaccio <nicolagallaccio240815@gmail.com>
2024-01-23 18:54:07 +0100

    fine esercizi

--- main.js
+++ main.js
@@ -417,3 +417,123 @@
 
 // console.log("Il numero massimo nell'array è:", massimo);
 // console.log("Il numero minimo nell'array è:", minimo);
+
+// **ESERCITAZIONE SUI METODI DEGLI ARRAY**
+
+// **ESERCIZIO 1:**
+
+// Dato un array di numeri, creare una funzione che restituisca la somma di tutti i numeri contenuti nell'array. Esempio: let array = [1, 5 ,7 ,12]; Output = 25;
+
+// let numbers = [1, 5, 7, 12];
+// function somma (array) {
+// let sum = array.reduce((acc, number )=> acc + number) 
+// return sum
+// }
+// somma(numbers);
+// console.log(somma(numbers));
+
+
+// **ESERCIZIO 2:**
+
+// Scrivi un programma che dato un array di numeri, restituisca in output la media e tutti i valori minori della media. Esempio: Input: a = [3, 5, 10, 2, 8] Output: media = 5.6, valori minori = [3, 5, 2]
+
+// function mediaEValoriMinori(array) {
+    // Calcola la media
+//     const media = array.reduce((acc, valore) => acc + valore, 0) / array.length;
+  
+    // Trova i valori minori della media
+//     const valoriMinori = array.filter(valore => valore < media);
+  
+    // Restituisce la media e i valori minori
+//     return { media, valoriMinori };
+//   }
+  
+  // Esempio di utilizzo
+//   const arrayInput = [3, 5, 10, 2, 8];
+//   const { media, valoriMinori } = mediaEValoriMinori(arrayInput);
+  
+  // Stampare i risultati
+//   console.log("Input:", arrayInput);
+//   console.log("Media:", media);
+//   console.log("Valori minori della media:", valoriMinori);
+
+// let numbers = [1, 5, 7, 12];
+// function somma (array) {
+// let sum = array.reduce((acc, number )=> acc + number/5) 
+// return sum
+// }
+// somma(numbers);
+// console.log(somma(numbers));
+
+// **ESERCIZIO 3:**
+
+// Scrivi un programma che dati  2 array di 10 elementi interi casuali compresi tra 1 e 10, permetta di effettuare, una delle seguenti operazioni: 
+
+// addizione
+
+// sottrazione 
+
+// moltiplicazione 
+
+// divisione
+
+// Esegua il calcolo tra ogni elemento dei due array, salvando ciascun risultato in un terzo array d’appoggio.
+
+// Esempio: Input: a= [3, 7, 2, 5, 8, 1, 2, 5, 6, 4], b= [9, 3, 1, 4, 7, 6, 5, 10, 1, 5], operazione = "addizione" Output: c = [12, 10, 3, 9, 15, 7, 7, 15, 7, 9]
+
+// **ESERCIZIO 4: **
+
+// Scrivere un programma che permetta di filtrare soltanto le parole all’interno di un array:
+
+// let mixArray = [1, true, ‘hackademy’, 100, ‘Javascript’, false, null, ‘php’]
+
+// il risultato dovra’ dare, let filtered = [‘hackademy’, ‘Javascript’, ‘php’]
+
+// **HINT:** provate a filtrare in base al tipo di dato (operatore typeof)
+
+// let mixArray= [[1, true, 'hackademy', 100, Javascript, false, null, 'php']];
+
+
+// const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+// const result = words.filter((word) => word.length >= 5);
+// console.log(words);
+
+
+
+
+
+// **EXTRA:**
+
+//  Scrivi una funzione che prenda in input una stringa e restituisca TRUE se è palindroma, FALSE se non lo è.
+
+// **Nel controllo scarta gli spazi e i segni di punteggiatura**
+
+//   Esempio:
+
+//   Input: “i topi non avevano nipoti”
+
+//   Output: TRUE
+
+// Suggerimento: Puoi eliminare spazi e segni di punteggiatura usando:
+// str.replace(/\W/g, "")
+
+
+// Spacchettate l 'esercizio in problemi più piccoli, e cercando su internet, trovate i metodi che vi servono!
+ 
+
+// function isPalindroma(stringa) {
+//     // Rimuovi spazi e segni di punteggiatura dalla stringa
+//     const stringaPulita = stringa.replace(/[^\w]/g, '').toLowerCase();
+  
+//     // Verifica se la stringa pulita è uguale al suo inverso
+//     return stringaPulita === stringaPulita.split('').reverse().join('');
+//   }
+  
+//   // Esempio di utilizzo
+//   const input = "i topi non avevano nipoti";
+//   const output = isPalindroma(input);
+  
+//   // Stampare il risultato
+//   console.log(`Input: "${input}"`);
+//   console.log("Output:", output);
+  
